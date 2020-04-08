7e762cc3c8f2abb46f2cca8c78c623d3f97c542e
Glen <mistergjones@yahoo.com.au>
2020-04-08 12:52:08 +1000

    all logic working. Need to do README.MD

--- tictactoe.js
+++ tictactoe.js
@@ -12,28 +12,39 @@ var answer0 = '000';
 
 var arrayInsertIdxPosition = null;
 
+// var btnZero = document.querySelector('#zero');
+// var btnOne = document.querySelector('#one');
+// var btnTwo = document.querySelector('#two');
+// var btnThree = document.querySelector('#three');
+// var btnFour = document.querySelector('#four');
+// var btnFive = document.querySelector('#five');
+// var btnSix = document.querySelector('#six');
+// var btnSeven = document.querySelector('#seven');
+// var btnEight = document.querySelector('#eight');
+var boxElements = document.querySelectorAll('.button');
+
+
+
 // 2. obtain all elements in the html page
-var boxElements = document.querySelectorAll("div");
+//var boxElements = document.querySelectorAll("div");
 var player1 = document.querySelector(".player1");
 var player2 = document.querySelector(".player2");
 var winOrDrawMessage = document.querySelector(".winOrDrawMessage");
-var resestButton = document.querySelector(".reset-btn");
+var resetButton = document.querySelector(".reset-btn");
 
 // 3. set the UI starting conditions
 player1.style.backgroundColor = "lightgreen";
-resestButton.disabled = false;
+resetButton.disabled = false;
+//resetButton.style.visibility = "hidden";
 
 // 4. Declare the functions used
 
 var handleClick = function (event) {
 
-    // event.target.style.backgroundColor = 'red';
+    //event.target.style.backgroundColor = 'red';
     event.target.classList.toggle('red');
-
-    //event.target.textContent = "-";
-    console.log(event.target.textContent = "X");
-    console.log(event.target.getAttribute("data-ArrayIdx"));
-
+    event.target.disabled = true;
+    
     // work out how to update the master grid array with the values
     var arrayInsertIdxPosition = Number(event.target.getAttribute("data-ArrayIdx"));
     //gameGridArray[arrayInsertIdxPosition] = event.target.textContent = "X";
@@ -65,6 +76,7 @@ var handleClick = function (event) {
             removeBoxElementsEventListener();
         }
     } else {
+        //event.target.classList.toggle('red');
         console.log("it does not work")
     }
 
@@ -94,6 +106,11 @@ var isPlayerOneTurn = function() {
 var populateDecisionArrays = function() {
 
     // require 8 by 3 rows for all combinations to check
+    tempc1 = null;
+    tempc2 = null;
+    tempc3 = null;
+    tempd1 = null;
+    tempd2 = null;
 
     // array methods to populate the row combinations and convert to strings.
     tempRow1 = gameGridArray.slice(0,3).join("");
@@ -131,7 +148,8 @@ var removeBoxElementsEventListener = function () {
     }
 }
 
-// thus function checks the combination and if there is a winner, will also then call and disable the 
+
+// This function checks the combination and if there is a winner, will also then call and disable the 
 // handle click event listener
 var checkWin = function (potentialWinner) {
 
@@ -151,7 +169,33 @@ var checkWin = function (potentialWinner) {
     }
 }
 
+
+var handleResetGame = function (event) {
+    //reset the array to blanks
+    gameGridArray = ["-","-","-","-","-","-","-","-","-"];
+
+    // reset the handleclick event listener and all UI elements
+    for (var i = 0; i < boxElements.length; i++) {
+        boxElements[i].textContent ="-"
+        //boxElements[i].style.backgroundColor = "lightgrey";
+        boxElements[i].disabled = false;
+        boxElements[i].classList.remove('red');
+        boxElements[i].classList.remove('green');
+        boxElements[i].addEventListener('click', handleClick);
+    }
+    // reset the main counter and reset message to blank       
+    counter = 0;
+    winOrDrawMessage.textContent = "";
+    player1.style.backgroundColor = "lightgreen";
+    player2.style.backgroundColor = "white";
+
+}
+
 //add an event click listener for all div elements on the game board
 for (var i = 0; i < boxElements.length; i++) {
     boxElements[i].addEventListener('click', handleClick);
+
 }
+
+// add an eveny click listens to reset the game if player wins or a draw
+resetButton.addEventListener('click', handleResetGame);
